5e638c74c0103f0ba25adf7c1abf26e769c2b087
Dragan <onchulee.wip@hotmail.com>
2021-03-01 13:01:16 +0700

    Add Switch System
    - Add 3 prototype switches for test

---
+++ VRdentist/Assets/Scenes/Fern/Scripts/SwitchController.cs
@@ -0,0 +1,56 @@
+﻿using com.dgn.UnityAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchController : MonoBehaviour
+{
+    public enum Activation { TurnOff, SwitchA, SwitchB }
+    public enum Axis { X, Y, Z }
+
+    [System.Serializable]
+    public struct MinMaxVector3
+    {
+        public Vector3 min;
+        public Vector3 max;
+    }
+
+    [System.Serializable]
+    public struct MinMax
+    {
+        public float min;
+        public float max;
+    }
+    
+    [SerializeField]
+    [ReadOnly]
+    protected Activation activation;
+
+    [Header("Setup")]
+    public UnityEvent onTurnOffEvent;
+    public UnityEvent onTurnSwitchAEvent;
+    public UnityEvent onTurnSwitchBEvent;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        activation = Activation.TurnOff;
+        onTurnOffEvent.Invoke();
+    }
+    
+    protected void ActivateSwitch(Activation activeSwitch) {
+        if (activation == activeSwitch) return;
+        activation = activeSwitch;
+        switch (activeSwitch)
+        {
+            case Activation.SwitchA:
+                onTurnSwitchAEvent.Invoke();
+                break;
+            case Activation.SwitchB:
+                onTurnSwitchBEvent.Invoke();
+                break;
+            default:
+                onTurnOffEvent.Invoke();
+                break;
+        }
+    }
+}
---
+++ VRdentist/Assets/Scenes/Fern/Scripts/SwitchSlide.cs
@@ -0,0 +1,121 @@
+﻿using com.dgn.UnityAttributes;
+using UnityEngine;
+
+public class SwitchSlide : SwitchController
+{
+    [ReadOnly]
+    [SerializeField]
+    private float velocity;
+    [ReadOnly]
+    [SerializeField]
+    private float direction;
+    
+    public Axis activationAxis;
+    public MinMax turnSwitchAVal;
+    public MinMax turnSwitchBVal;
+
+    [Header("Switch Control")]
+    public Rigidbody lockBody;
+    public Vector3 defaultPosition;
+    public MinMaxVector3 limitPositions;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    void LateUpdate()
+    {
+        if (lockBody)
+        {
+            LockTransform();
+            CheckActivation(lockBody.transform.localPosition);
+        }
+    }
+
+    private void CheckActivation(Vector3 position)
+    {
+        switch (activationAxis)
+        {
+            case Axis.X:
+                CallEvent(position.x);
+                break;
+            case Axis.Y:
+                CallEvent(position.y);
+                break;
+            case Axis.Z:
+                CallEvent(position.z);
+                break;
+        }
+    }
+
+    private void CallEvent(float val)
+    {
+        if (val == Mathf.Clamp(val, turnSwitchAVal.min, turnSwitchAVal.max))
+        {
+            ActivateSwitch(Activation.SwitchA);
+        }
+        else if (val == Mathf.Clamp(val, turnSwitchBVal.min, turnSwitchBVal.max))
+        {
+            ActivateSwitch(Activation.SwitchB);
+        }
+        else
+        {
+            ActivateSwitch(Activation.TurnOff);
+        }
+    }
+
+    private void LockTransform()
+    {
+        lockBody.transform.localPosition = Vector3Extensions.PositionClamp(lockBody.transform.localPosition, limitPositions.min, limitPositions.max);
+        // limit velocity
+        float limitV = Mathf.Clamp01(lockBody.velocity.magnitude);
+        lockBody.velocity = lockBody.velocity.normalized * limitV;
+        // no spring bounce
+        velocity = GetVelocity();
+        direction = GetDirection();
+        if (velocity > 0 && direction > 0)
+        {
+            lockBody.velocity = Vector3.zero;
+            lockBody.transform.localPosition = defaultPosition;
+        }
+        if (velocity < 0 && direction < 0)
+        {
+            lockBody.velocity = Vector3.zero;
+            lockBody.transform.localPosition = defaultPosition;
+        }
+    }
+
+    private float GetVelocity() {
+        if (lockBody) {
+            switch (activationAxis)
+            {
+                case Axis.X:
+                    return lockBody.velocity.x;
+                case Axis.Y:
+                    return lockBody.velocity.y;
+                case Axis.Z:
+                    return lockBody.velocity.z;
+            }
+        }
+        return 0f;
+    }
+
+    private float GetDirection()
+    {
+        if (lockBody)
+        {
+            switch (activationAxis)
+            {
+                case Axis.X:
+                    return lockBody.transform.localPosition.x - defaultPosition.x;
+                case Axis.Y:
+                    return lockBody.transform.localPosition.y - defaultPosition.y;
+                case Axis.Z:
+                    return lockBody.transform.localPosition.z - defaultPosition.z;
+            }
+        }
+        return 0f;
+    }
+
+}

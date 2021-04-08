bbd147e514dc0748aa19d6a15c68c6e01c8470c3
Dragan <onchulee.wip@hotmail.com>
2021-04-08 12:00:57 +0700

    Update Model Room
    Fix UV + add Glass

---
+++ VRdentist/Assets/Scripts/GumController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GumController : MonoBehaviour
+{
+    [System.Serializable]
+    public struct StitchArea {
+        public Transform spot1;
+        public Transform spot2;
+    }
+
+    [SerializeField]
+    public StitchArea[] stitchAreas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
